59e811fc5c1d7cf4fd3fc790b98d57ddb7049c8f
RachitaHajela <rachitahajela.25@gmail.com>
2016-08-04 10:24:05 -0700

    first commit for the sample weather app in angular

---
+++ app/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngMaterial',
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]).
+    controller('AppCtrl', function($scope) {
+        $scope.title1 = 'Hello';
+        $scope.title4 = 'Warn';
+        $scope.isDisabled = true;
+        $scope.googleUrl = 'http://google.com';
+});

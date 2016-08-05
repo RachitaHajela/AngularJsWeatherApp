9bd472ac7889b080ece4fe23a6e5045239fc7f72
git-ketan <kg1689@nyu.edu>
2016-08-05 10:08:15 -0400

    Added Open-Weather-API App ID and JSONP requests integration. Sample Jsonp request is running and data shown on web page index.html.

--- app/app.js
+++ app/app.js
@@ -19,8 +19,19 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
         $scope.isDisabled = true;
         $scope.googleUrl = 'http://google.com';
 
-        $http.get("http://ip.jsontest.com/?callback=showMyIP")
+       /* $http.get("http://ip.jsontest.com/?callback=showMyIP")
         .then(function(response) {
-            $scope.myWelcome = response.data;
-        });
+            $scope.myWelcome = {"ip": "216.2.193.1"};
+            //response.data;
+        });*/
+
+    var url = "http://api.openweathermap.org/data/2.5/weather?APPID=70927e3ee92e2348e18e53fd5a9d9afd&q=NewYork&callback=JSON_CALLBACK";
+
+    $http.jsonp(url).
+    success(function(data, status, headers, config) {
+        $scope.myWelcome = data;
+    }).
+    error(function(data, status, headers, config) {
+        $scope.error = true;
+    });
 });
